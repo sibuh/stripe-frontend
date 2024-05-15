ab2e8a885b507b28f90f365a5bdeef0e25cb44ea
misganaw <misgandess@gmail.com>
2024-05-15 18:14:20 +0300

    react code for stripe

---
+++ src/Payment.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { useParams } from "react-router-dom";
+
+function Payment() {
+  const [stripePromise, setStripePromise] = useState(null);
+  const [clientSecret, setClientSecret] = useState("");
+  const { productId } = useParams();
+
+  useEffect(() => {
+    fetch("http://localhost:8000/pk").then(async (r) => {
+      const { publishableKey } = await r.json();
+      setStripePromise(loadStripe(publishableKey));
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/mido/cpi", {
+      method: "POST",
+      body: JSON.stringify({
+        id: parseInt(productId),
+      }),
+    }).then(async (result) => {
+      var { clientSecret } = await result.json();
+      setClientSecret(clientSecret);
+    });
+  }, [productId]);
+
+
+  return (
+    <div style={{}}>
+      <h1>React Stripe and the Payment Element</h1>
+      {clientSecret && stripePromise && (
+        <Elements
+          stripe={stripePromise}
+          options={{ clientSecret, locale: "ja" }}
+        >
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
+
+export default Payment;
---
+++ src/Products.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Products = () => {
+  const [products, setProducts] = useState();
+  const history = useNavigate();
+
+  useEffect(() => {
+    fetch("http://localhost:3001/mido/products").then(async (r) => {
+      const { products } = await r.json();
+      console.log("first", products);
+      setProducts(products);
+    });
+  }, []);
+
+  const handleBuy = (id) => {
+    history(`/payment/${id}`);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2>Buy Products Here</h2>
+      <div style={{ display: "flex", marginTop: "20px" }}>
+        <br />
+        {products?.map((product, i) => {
+          return (
+            <div style={{ padding: "50px" }}>
+              <div style={{ width: "100%" }}>
+                <h3>{product?.title}</h3>
+                <h3>{product?.description}</h3>
+                <h3>
+                  <strong>${product?.price}</strong>
+                </h3>
+                <br />
+              </div>
+              <button onClick={() => handleBuy(product?.id)}>Buy now</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
