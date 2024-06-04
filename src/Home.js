46a871c3f9e735c01ea6da600f623547e05fa7c0
misganaw <misgandess@gmail.com>
2024-06-04 22:43:51 +0300

    added logo image

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from './images/logo';
 const Home = () => {
     return ( 
         <div className="home-page">
-            <h1 className="home-page-title">Events you do not want to miss</h1>
+            <div className="header">
+                <img src={logo} /> <h1 className="home-page-title">Events you do not want to miss</h1>
+            </div>
+           
             <ul className="event-list">
                 <li>Music Concert</li>
                 <li>Fund Raising</li>
